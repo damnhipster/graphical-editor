082ff467b1fa35c4ed2634af38573fa0978fd142
damnhipster <hamant.brahmbhatt@gmail.com>
2014-07-19 19:36:40 +0100

    Moved command parsing logic into a parse method

--- command.rb
+++ command.rb
@@ -10,15 +10,17 @@ class Command
     quit: 'X'
   }
 
-  def self.parse(command)
-    self.new(command.split(' '))
+  def self.parse(user_command)
+    command = user_command.split(' ')
+    instruction = @@instructions.key(command[0])
+    row = command[1].to_i
+    column = command[2].to_i
+    color = command[3] || 'O'
+    self.new(instruction, row, column, color)
   end
 
-  def initialize(command)
-    @instruction = @@instructions.key(command[0])
-    @row = command[1].to_i
-    @column = command[2].to_i
-    @color = command[3] || 'O'
+  def initialize(instruction, row, column, color)
+    @instruction, @row, @column, @color = instruction, row, column, color
   end
 
 end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -3,14 +3,14 @@ require_relative '../command'
 describe Command do
 
   it "should parse a create command" do
-    command = Command.new(%w[ I 3 2 ])
+    command = Command.parse('I 3 2')
     expect(command.instruction).to eq :new_image
     expect(command.row).to eq 3
     expect(command.column).to eq 2
   end
 
   it "should parse a color command" do
-    command = Command.new(%w[ L 1 2 A ])
+    command = Command.parse('L 1 2 A')
     expect(command.instruction).to eq :color_pixel
     expect(command.row).to eq 1
     expect(command.column).to eq 2
@@ -18,17 +18,17 @@ describe Command do
   end
 
   it "should parse a clear command" do
-    command = Command.new(%w[ C ])
+    command = Command.parse('C')
     expect(command.instruction).to eq :clear_image
   end
 
   it "should parse a show command" do
-    command = Command.new(%w[ S ])
+    command = Command.parse('S')
     expect(command.instruction).to eq :show_image
   end
 
   it "should parse an exit command" do
-    command = Command.new(%w[ X ])
+    command = Command.parse('X')
     expect(command.instruction).to eq :quit
   end
 
